86f58311caeb8350107177157403ad3592e43b86
Tanmay Arya <tanmay@Tanmays-MacBook-Pro.local>
2021-12-26 23:11:10 +0530

    practice questions

--- src/JavaBasics/PracticeQuestions.java
+++ src/JavaBasics/PracticeQuestions.java
@@ -20,32 +20,57 @@ public class PracticeQuestions {
 
 		
 		// Find if a number is Prime or not
-		  int T = scn.nextInt();
-		  int i = 1;
-
-		  while(i <= T) {
-
-			  int num = scn.nextInt();
-			  int val = 2;
-			  boolean isPrime = true;
-			
-			while(val * val <= num) {
-				if(num % val == 0) {
-					isPrime = false;
-					break;
-				}
-				
-				val = val + 1;
-			}
-
-			if(isPrime == true) {
-				System.out.println("Yes");
-			} else {
-				System.out.println("No");
-			}
-
-			  i = i+1;
-		  }		
+//		  int T = scn.nextInt();
+//		  int i = 1;
+//
+//		  while(i <= T) {
+//
+//			  int num = scn.nextInt();
+//			  int val = 2;
+//			  boolean isPrime = true;
+//			
+//			while(val * val <= num) {
+//				if(num % val == 0) {
+//					isPrime = false;
+//					break;
+//				}
+//				
+//				val = val + 1;
+//			}
+//
+//			if(isPrime == true) {
+//				System.out.println("Yes");
+//			} else {
+//				System.out.println("No");
+//			}
+//
+//			  i = i+1;
+//		  }		
+		
+		// count number of primes from 1 to n
+//		int n = scn.nextInt();
+//
+//        int count = 0;
+//        int numToCheck = 2;
+//
+//        while (numToCheck <= n) {
+//              boolean isPrime = true;
+//              int val = 2;
+//              while (val * val <= numToCheck) {
+//                    if(numToCheck % val == 0) {
+//                          isPrime = false;
+//                          break;
+//                    }
+//                    val = val + 1;
+//              }
+//              if(isPrime == true) {
+//                    count = count + 1;
+//              }
+//              numToCheck = numToCheck + 1;
+//        }  
+//        System.out.println(count);         
+//		
+		
 		
 		
 	}
