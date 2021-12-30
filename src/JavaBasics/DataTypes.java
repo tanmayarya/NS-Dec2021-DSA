36e6602983b6233849e65d1c7ad9284ce43424bf
Tanmay Arya <tanmay@192.168.1.36>
2021-12-30 23:32:35 +0530

    operators

--- src/JavaBasics/DataTypes.java
+++ src/JavaBasics/DataTypes.java
@@ -32,63 +32,64 @@ public class DataTypes {
 				by = (byte)130;
 				System.out.println(by);
 				
-//				// Part 3;
-//				in = 1000000000;
-////				in = 10000000000;
-////				in = 10000000000L;
-//				in = (int)10000000000L;
-//				
-//				// Part 4
-////				float fl = 5.5;
-//				float fl = 5.5F;
-//				double db = 5.5;
-//				
-////				fl = db;
-//				fl = (float)db;
-//				db = fl;
-//				
-//				// Part 5
-////				in = fl;
-//				in = (int)fl;
-//				
-//				// Part 6
-//				boolean bo = true;
-//				bo = false;
-//				
-//				if(bo){
-//					System.out.println("Hi");
+				// Part 3;
+				in = 1000000000;
+//				in = 10000000000;
+//				in = 10000000000L;
+				in = (int)10000000000L;
+				
+				// Part 4
+//				float fl = 5.5;
+				float fl = 5.2323235F;
+				double db = 235.5435345;
+				
+//				fl = db;
+				fl = (float)db;
+				db = fl;
+				
+				// Part 5
+//				in = fl;
+				in = (int)fl;
+				
+				// Part 6
+				boolean bo = true;
+				bo = false;
+				
+				if(bo){
+					System.out.println("Hi");
+				}
+				else {
+					System.out.println("Bye");
+				}
+				
+//				if(in){
+//					System.out.println("Bizzarre");
 //				}
-//				else {
-//					System.out.println("Bye");
-//				}
-//				
-////				if(in){
-////					System.out.println("Bizzarre");
-////				}
-//				
-//				if(in > 0){
-//					System.out.println("Ok");
-//				}
-//				
-//				// Part 7
-//				char ch = 'a';
-//				ch = 97;
-//				ch = 64000;
-////				ch = 67000;
-//				ch = (char)67000;
-//				
-//				ch = (char)(Math.pow(2, 16) - 1 + 98);
-//				System.out.println(ch);
-//				
-//				in = ch + 23;
-////				ch = ch + 1;
-//				
-//				// Part 8
-//				System.out.println(10 + 20 + "Hello" + 10 + 20);
-//				System.out.println(2 + ' ' + 5);
-//				System.out.println(2 + " " + 5);
-//				System.out.println("Hello" + '\t' + "World");
-//				System.out.println("Hello" + "\t" + "World");
+				
+				if(in > 0){
+					System.out.println("Ok");
+				}
+				
+				// Part 7
+				char ch = '0';
+				ch = 97;
+				ch = 64000;
+//				ch = 67000;
+				ch = (char)67000;
+				
+				ch = (char)(Math.pow(2, 16) - 1 + 98);
+				System.out.println(ch);
+				
+				in = ch + 23;
+//				ch = ch + 1;
+				System.out.println(in);
+				// Part 8
+				System.out.println(10 + 20 + "Hello" + 10 + 20);// "30Hello1020"
+				System.out.println(2 + ' ' + 5);
+				System.out.println(2 + " " + 5); // "2  5"
+				System.out.println("Hello" + '\t' + "World");
+				System.out.println("Hello" + "\t" + "World");
+				System.out.println("Hello" + "\n" + "World");
 		
 	}
 }
---
+++ src/JavaBasics/Operators.java
@@ -0,0 +1,54 @@
+package JavaBasics;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		// unary operators
+		// increment
+		int a = 5;
+		int b = ++a;
+		int c = a++;
+		int d = ++c;
+		
+//		System.out.println("a = " + a);
+//		System.out.println("b = " + b);
+//		System.out.println("c = " + c);
+//		System.out.println("d = " + d);
+		
+		
+		//decrement
+		a = 5;
+		b = --a;
+		c = a--;
+		d = --c;
+		
+//		System.out.println("a = " + a);
+//		System.out.println("b = " + b);
+//		System.out.println("c = " + c);
+//		System.out.println("d = " + d);
+		a = 5;
+		b = 2;
+		c = 5;
+		d = 7;
+		
+		
+		// Relational Operators
+		// < , > , <= , >=, ==, !=
+//		System.out.println(a > b);
+//		System.out.println(a >= b);
+//		System.out.println(a < b);
+//		System.out.println(a != b);
+//		System.out.println(a == c);
+		
+		// logical operators
+		// && AND
+		// || OR
+//		System.out.println(true && true);
+//		System.out.println(false && true);
+//		System.out.println(false || true);
+//		System.out.println(a > b && c > b && a == c);
+
+	}
+
+}
