10e4c544b7611c6be9cdc1707b6f471d6bf9dc6a
Tanmay Arya <tanmay@Tanmays-MacBook-Pro.local>
2022-01-11 23:11:55 +0530

    array questions

--- src/Arrays/Questions.java
+++ src/Arrays/Questions.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Questions {
 	
 	public static int getMax(int arr[]) {
@@ -56,6 +58,51 @@ public class Questions {
 		return -1;
 	}
 	
+	public static int[] reverse(int arr[]) {
+		
+		int l = 0, r = arr.length - 1;
+		
+		while(l < r) {
+			// swap
+			int temp = arr[l];
+			arr[l] = arr[r];
+			arr[r] = temp;
+			
+			l++;
+			r--;
+		}
+		return arr;
+	}
+	
+	public static void display(int arr[]) {
+		for(int i = 0; i < arr.length; i++) 
+			System.out.println(arr[i]);
+		
+	}
+	
+	public static void pairSum(int arr[], int k) {
+		
+		for(int i = 0; i < arr.length - 1; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[i] + arr[j] == k) 
+					System.out.println(arr[i] + ", " + arr[j]);
+			}
+		}
+	}
+	
+	public static void tripletSum(int arr[], int target) {
+		
+		for(int i = 0; i < arr.length - 2; i++) {
+			for(int j = i + 1; j < arr.length - 1; j++) {
+				for(int k = j + 1; k < arr.length; k++) {
+					if(arr[i] + arr[j] + arr[k] == target) {
+						System.out.println(arr[i] + ", " + arr[j] + ", " + arr[k]);
+					}
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		int a[] = {10, 20, -50, 100, 60, -200, 20};
@@ -63,9 +110,23 @@ public class Questions {
 //		OddEvenSum(a);
 //		System.out.println(isElementRepeated(a, 20));
 		
-		int arr[] = {10, 20 ,30 ,1, 40, 30, 20, 60};
+//		Scanner scn = new Scanner(System.in);
+//		int n = scn.nextInt();
+		int arr[] = {10, 20 ,30 ,1, 40, 30, 20, 49};
+//		int arr[] = new int [n];
+//		
+//		for(int i = 0; i < n; i++) 
+//			arr[i] = scn.nextInt();
+			
 		
-		System.out.println(repeatedNumber(arr));
 		
+//		System.out.println(repeatedNumber(arr));
+		
+//		int arr2[] = reverse(arr);
+//		display(arr2);
+		
+//		pairSum(arr, 50);
+		int arr2[] = {4, 8, 1, 6, 5, 3, 2};
+		tripletSum(arr2, 9);
 	}
 }
